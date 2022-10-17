bb4f2ae5125a55948d58fb7bae6fd1af0b75699a
Letícia de Lima da Cunha <111303568+Leticia-LC@users.noreply.github.com>
2022-10-17 15:10:48 -0300

    Add files via upload

---
+++ MainPessoa.java
@@ -0,0 +1,18 @@
+package ifsc;
+
+public class MainPessoa {
+
+	public static void main(String[] args) {
+		Professor professor = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		professor.setSiape(1254);
+		
+		pessoa.setNome("Bruna");
+		pessoa.setCpf("234543");
+		
+
+	}
+
+}
