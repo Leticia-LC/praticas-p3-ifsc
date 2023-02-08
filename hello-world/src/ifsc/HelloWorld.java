10c131e20bb95d1689f515aec07b1bf6bddb8aee
Leticia <leticia.lc2005@aluno.ifsc.edu.br>
2023-02-08 16:53:16 -0300

    primeira prática de java :)

---
+++ hello-world/src/ifsc/HelloWorld.java
@@ -0,0 +1,25 @@
+package ifsc;
+import java.util.Scanner;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		Scanner leitura = new Scanner(System.in);
+		
+		System.out.println("Nome: ");
+		
+		String nome = leitura.nextLine();
+		
+		System.out.println("Idade: ");
+		String idadeStr = leitura.nextLine();
+		
+		int idade = Integer.valueOf(idadeStr);
+
+		System.out.println(nome + " tem " + idade);
+		
+		}
+
+	}
+
+
